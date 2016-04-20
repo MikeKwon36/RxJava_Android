f545e4ae531a8c9aca0393ee7e31da8b46432184
MikeKwon36 <Mikeykwon@gmail.com>
2016-04-20 18:56:36 -0400

    update

--- app/src/main/java/com/kwondeveloper/rxpractice/MainActivity.java
+++ app/src/main/java/com/kwondeveloper/rxpractice/MainActivity.java
@@ -8,6 +8,7 @@ import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         //example 1 (more Java oriented)==========================================================
         hello("Ben", "George");
 
+
         //example 2 (more Android oriented)=======================================================
         Observable<String> myObservable = Observable.just("Hello"); // Emits "Hello"
-
         Observer<String> myObserver = new Observer<String>() {
             @Override
             public void onCompleted() {
@@ -38,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         Subscription subscription1 = myObservable.subscribe(myObserver);
-
         Action1<String> myAction = new Action1<String>() {
             @Override
             public void call(String s) {
@@ -46,9 +46,41 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         Subscription subscription2 = myObservable.subscribe(myAction);
-
+        subscription1.unsubscribe();
         subscription2.unsubscribe();
+
+        //example 3 (operators)==================================================================
+
+        Observable<Integer> myArrayObservable
+                = Observable.from(new Integer[]{1, 2, 3, 4, 5, 6}); // Emits each item of the array, one at a time
+        myArrayObservable.subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer i) {
+                Log.d("My Action", String.valueOf(i)); // Prints the number received
+            }
+        });
+
+        //map operator returns a new Observable, rather than change the original
+        myArrayObservable = myArrayObservable.map(new Func1<Integer, Integer>() { // Input and Output are both Integer
+            @Override
+            public Integer call(Integer integer) {
+                return integer * integer; // Square the number
+            }
+        });
+
+        //operators can also be chained, where the following code block uses the skip operator
+        // to skip the first two numbers, and then the filter operator to ignore odd numbers
+        myArrayObservable
+                .skip(2) // Skip the first two items
+                .filter(new Func1<Integer, Boolean>() {
+                    @Override
+                    public Boolean call(Integer integer) { // Ignores any item that returns false
+                        return integer % 2 == 0;
+                    }
+                });
     }
+
+
 
     public static void hello(String... names) {
         Observable.from(names).subscribe(new Action1<String>() {
